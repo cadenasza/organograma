37f4b58f7944f8086621636be83dc2c07bc56c21
cadenasza <leonardocadena2005@gmail.com>
2024-12-18 18:36:29 -0300

    adicionando o banner e apagando coisas que nao iremos utilizar

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import "./Banner.css"
+
+export default function Banner() {
+  return (
+    <header className="banner">
+        <img src="/img/banner.png" alt="banner principal pagina do Organo" />
+    </header>
+  )
+}
